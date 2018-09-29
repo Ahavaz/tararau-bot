8995d93dd178b1370a264927a499f5a8d7963bc9
Ahavaz <lucas.cesar.8@hotmail.com>
2018-09-29 00:46:42 -0300

    added basic chat structure

--- index.js
+++ index.js
@@ -1,2 +1,70 @@
-const TOKEN = process.env.TELEGRAM_CHATBOT_API_KEY
+process.env.NTBA_FIX_319 = 1
+const TelegramBot = require('node-telegram-bot-api')
+const token = process.env.TELEGRAM_CHATBOT_API_KEY || '689490724:AAGkfEPpeptNtBYVESJh_iO0b46dtOReNVo'
+const bot = new TelegramBot(token, { polling: true })
 
+bot.on('message', msg => {
+  const userMsg = msg.text.toString().toLowerCase()
+
+  const tararau = /ta+ra+ra+u+/
+  const ayn = /a+y+n+/
+  const kkk = /kk+/
+  const vei = ['vei', 'mano']
+  const greetings = ['oi', 'oir', 'oie', 'oe', 'oer', 'olá', 'e ae', 'fala ae', 'falae', 'olar', 'hello', 'hey', 'hey there', 'hallo', 'hola', 'salut']
+  const farewells = ['tchau', 'xau', 'xaus', 'flw', 'flws', 'vlw flw', 'adios', 'adeus', 'bye', 'goodbye', 'fui', 'até', 'ateh', 'até já', 'ateh jah', 'ateh ja', 'até mais', 'ateh mais', 'até logo', 'ateh logo', 'cya', 'see ya', 'see you', 'hasta la vista']
+  const swearings = [' da puta', 'fdp', 'se foder', 'te foder', 'se foderem', 'te foderem', 'vsf', ' no cu', 'tnc']
+  const ow = ['ow', 'ei', 'psiu']
+  const shit = ['tolete', 'merda', 'bosta', 'cocô', 'shit', 'caguei', 'cagou', 'cagaram', 'cagando', 'cagar']
+
+  const greetMatches = greetings.filter(message => 
+    userMsg === message 
+    || userMsg.startsWith(message + ' ')
+    || userMsg.startsWith(message + ',')
+    || userMsg.startsWith(message + '.')
+    || userMsg.startsWith(message + ';')
+    || userMsg.startsWith(message + ':')
+    || userMsg.startsWith(message + '!')
+    || userMsg.startsWith(message + '?'))
+
+  const farewellMatches = farewells.filter(message =>
+    userMsg === message
+    || userMsg.startsWith(message + ' ')
+    || userMsg.startsWith(message + ',')
+    || userMsg.startsWith(message + '.')
+    || userMsg.startsWith(message + ';')
+    || userMsg.startsWith(message + ':')
+    || userMsg.startsWith(message + '!')
+    || userMsg.startsWith(message + '?'))
+
+  const swearingsMatches = swearings.filter(message => userMsg.includes(message))
+
+  const owMatches = ow.filter(message => userMsg === message)
+
+  const shitMatches = shit.filter(message => userMsg.includes(message))
+
+  const veiMatches = vei.filter(message => userMsg === message)
+
+  if (tararau.test(userMsg)) {
+    bot.sendMessage(msg.chat.id, 'TARARAAAAAU')
+  } else if (ayn.test(userMsg)) {
+    bot.sendMessage(msg.chat.id, 'Ayn')
+  } else if (kkk.test(userMsg)) {
+    bot.sendMessage(msg.chat.id, 'Hahaha')
+  } else if (veiMatches.length !== 0) {
+    bot.sendMessage(msg.chat.id, 'Diga')
+  } else if (greetMatches.length !== 0) {
+    bot.sendMessage(msg.chat.id, 'E ae, Tararau!')
+  } else if (farewellMatches.length !== 0) {
+    bot.sendMessage(msg.chat.id, 'Vlw flw')
+  } else if (swearingsMatches.length !== 0) {
+    bot.sendMessage(msg.chat.id, 'Mas que boca suja é essa?!')
+  } else if (owMatches.length !== 0) {
+    bot.sendMessage(msg.chat.id, 'Hm')
+  } else if (shitMatches.length !== 0) {
+    bot.sendMessage(msg.chat.id, 'CAGUEI!')
+  }
+})
+
+bot.onText(/\/role/, msg => {
+  bot.sendMessage(msg.chat.id, 'Quando vocês querem meter o loko?')
+})
