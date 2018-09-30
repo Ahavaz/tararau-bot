d15e03c677e55684e1433d151b22b29baa0fe797
Ahavaz <lucas.cesar.8@hotmail.com>
2018-09-30 19:26:59 -0300

    defined parameters

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ process.env.NTBA_FIX_319 = 1
 const TelegramBot = require('node-telegram-bot-api')
 const token = process.env.TELEGRAM_CHATBOT_API_KEY
 const bot = new TelegramBot(token, { polling: true })
-
+const parameters = ''
 const url = `https://maps.googleapis.com/maps/api/geocode/json?${parameters}&key=${GOOGLE_API_KEY}`
 
 const filterMsg = (userMsg, options) => 
