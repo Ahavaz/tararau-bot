b76675eaf8fc98f88772c3f7444cb53790ac565f
Ahavaz <lucas.cesar.8@hotmail.com>
2018-09-29 22:32:33 -0300

    added code for testing purposes

--- index.js
+++ index.js
@@ -61,4 +61,22 @@ bot.on('message', msg => {
 
 bot.onText(/\/role/, msg => {
   bot.sendMessage(msg.chat.id, 'Quando vocês querem meter o loko?')
+
+  // const opts = {
+  //   reply_markup: JSON.stringify({
+  //     keyboard: [
+  //       [{ text: 'Location', request_location: true }],
+  //       [{ text: 'Contact', request_contact: true }],
+  //     ],
+  //     resize_keyboard: true,
+  //     one_time_keyboard: true,
+  //   })
+  // }
+
+  // bot.sendMessage(msg.chat.id, 'Contact and Location request', opts)
+
+  // bot.on('location', msg => {
+  //   console.log(msg.location.latitude)
+  //   console.log(msg.location.longitude)
+  // })
 })
