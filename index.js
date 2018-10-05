0711171635be617008af1cef01dcebc8f229dd8b
Ahavaz <lucas.cesar.8@hotmail.com>
2018-10-05 16:20:48 -0300

    Edited date display in role options

--- index.js
+++ index.js
@@ -76,11 +76,11 @@ const buildMsg = array => {
 
 const getNext = date => {
   date.add(1, 'days')
-  return `${date.format('dddd')} (${date.format('D MMM YY')})`
+  return `${date.format('dddd')} (${date.format('D/MMM/YY')})`
 }
 
 const buildDayOptions = date => [
-  [`Hoje (${date.format('D MMM')})`, `Amanhã (${date.add(1, 'days').format('D MMM')})`],
+  [`Hoje (${date.format('D/MMM/YY')})`, `Amanhã (${date.add(1, 'days').format('D/MMM/YY')})`],
   [`${getNext(date)}`, `${getNext(date)}`],
   [`${getNext(date)}`, `${getNext(date)}`],
   [`${getNext(date)}`, `Outra data`]
