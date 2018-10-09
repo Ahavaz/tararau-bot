3597ea570c04fe20808b7a13510b10e552d496a0
Ahavaz <lucas.cesar.8@hotmail.com>
2018-10-08 21:27:15 -0300

    Fixing #12

--- index.js
+++ index.js
@@ -40,142 +40,89 @@ bot.on('message', msg => {
   return true
 })
 
+const customKb = (msgId, customKeyboard) => ({
+  reply_to_message_id: msgId,
+  parse_mode: 'Markdown',
+  reply_markup: {
+    keyboard: customKeyboard,
+    force_reply: true,
+    resize_keyboard: true,
+    one_time_keyboard: true,
+    selective: true
+  }
+})
+
+const defaultKb = (msgId, isReply = false) => ({
+  reply_to_message_id: msgId,
+  parse_mode: 'Markdown',
+  reply_markup: {
+    force_reply: isReply,
+    remove_keyboard: true,
+    selective: true
+  }
+})
+
+const isValidDate = date =>
+  moment(date, 'D/M/YY', 'pt-br', true).isValid() && moment().isSameOrBefore(moment(date, 'D/M/YY'), 'day')
+
 bot.onText(/^\/role\b/i, msg => {
-  bot
-    .sendMessage(msg.chat.id, 'Quando vocês querem meter o loko?', {
-      reply_to_message_id: msg.message_id,
-      reply_markup: {
-        force_reply: true,
-        keyboard: buildDayOptions(moment()),
-        resize_keyboard: true,
-        one_time_keyboard: true,
-        selective: true
+  const chatId = msg.chat.id
+  const userId = msg.from.id
+  const msgId = msg.message_id
+  const callbackId = `${chatId}:${userId}`
+
+  bot.sendMessage(chatId, 'Quando vocês querem meter o loko?', customKb(msgId, buildDayOptions(moment()))).then(() => {
+    answerCallbacks[callbackId] = answerRoleDate => {
+      const answerRoleDateId = answerRoleDate.message_id
+
+      if (answerRoleDate.text === 'Outra data') {
+        bot.sendMessage(chatId, 'Digite uma data (DD/MM/AA) futura', defaultKb(answerRoleDateId, true)).then(() => {
+          answerCallbacks[callbackId] = answerAnotherDate => {
+            const answerAnotherDateId = answerAnotherDate.message_id
+            if (isValidDate(answerAnotherDate.text)) {
+              const date = moment(answerAnotherDate.text, 'D/M/YY')
+
+              bot.sendMessage(
+                chatId,
+                `${date.format('DD/MM/YY [(]dddd[)]').toLowerCase()}, qual horário (HH:mm)?`,
+                defaultKb(answerAnotherDateId, true)
+              )
+            } else {
+              bot.sendMessage(
+                chatId,
+                'Data inválida, escolha uma data futura e preste atenção no formato',
+                defaultKb(answerAnotherDateId)
+              )
+            }
+          }
+        })
+      } else if (answerRoleDate.text === 'Mudei de ideia') {
+        bot.sendMessage(
+          chatId,
+          `Vai ti toma no cu então ${answerRoleDate.from.first_name} ${emoji.find('upside_down_face').emoji}`,
+          defaultKb(answerRoleDateId)
+        )
+      } else if (moment(answerRoleDate.text.split('\n')[1].slice(1, -1), 'D/MMM/YY', 'pt-br', true).isValid()) {
+        const date = moment(answerRoleDate.text.split('\n')[1].slice(1, -1), 'D/MMM/YY')
+        bot.sendMessage(
+          chatId,
+          `${date.format('DD/MM/YY [(]dddd[)]').toLowerCase()}, qual horário (HH:mm)?`,
+          defaultKb(answerRoleDateId, true)
+        )
+      } else {
+        bot.sendMessage(
+          chatId,
+          `Use os botões, energúmeno ${emoji.find('face_with_rolling_eyes').emoji}`,
+          defaultKb(answerRoleDateId)
+        )
       }
-    })
-    .then(() => {
-      answerCallbacks[`${msg.chat.id}:${msg.from.id}`] = answerRoleDate => {
-        if (answerRoleDate.text === 'Outra data') {
-          bot
-            .sendMessage(msg.chat.id, 'Digite uma data (DD/MM/AA) futura', {
-              reply_to_message_id: answerRoleDate.message_id,
-              reply_markup: {
-                force_reply: true,
-                remove_keyboard: true,
-                selective: true
-              }
-            })
-            .then(() => {
-              answerCallbacks[
-                `${msg.chat.id}:${msg.from.id}`
-              ] = answerAnotherDate => {
-                if (
-                  moment(
-                    answerAnotherDate.text,
-                    'D/M/YY',
-                    'pt-br',
-                    true
-                  ).isValid() &&
-                  moment().isSameOrBefore(
-                    moment(answerAnotherDate.text, 'D/M/YY'),
-                    'day'
-                  )
-                ) {
-                  const date = moment(answerAnotherDate.text, 'D/M/YY')
-                  bot.sendMessage(
-                    msg.chat.id,
-                    `${date
-                      .format('DD/MM/YY [(]dddd[)]')
-                      .toLowerCase()}, qual horário (HH:mm)?`,
-                    {
-                      reply_to_message_id: answerAnotherDate.message_id,
-                      reply_markup: {
-                        remove_keyboard: true,
-                        selective: true
-                      }
-                    }
-                  )
-                } else {
-                  bot.sendMessage(
-                    msg.chat.id,
-                    'Data inválida, escolha uma data futura e preste atenção no formato',
-                    {
-                      reply_to_message_id: answerAnotherDate.message_id,
-                      reply_markup: {
-                        remove_keyboard: true,
-                        selective: true
-                      }
-                    }
-                  )
-                }
-              }
-            })
-        } else if (answerRoleDate.text === 'Mudei de ideia') {
-          bot.sendMessage(
-            msg.chat.id,
-            `Vai ti toma no cu então ${answerRoleDate.from.first_name} ${
-              emoji.find('upside_down_face').emoji
-            }`,
-            {
-              reply_to_message_id: answerRoleDate.message_id,
-              reply_markup: {
-                remove_keyboard: true,
-                selective: true
-              }
-            }
-          )
-        } else if (
-          moment(
-            answerRoleDate.text.split('\n')[1].slice(1, -1),
-            'D/MMM/YY',
-            'pt-br',
-            true
-          ).isValid()
-        ) {
-          const date = moment(
-            answerRoleDate.text.split('\n')[1].slice(1, -1),
-            'D/MMM/YY'
-          )
-          bot.sendMessage(
-            msg.chat.id,
-            `${date
-              .format('DD/MM/YY [(]dddd[)]')
-              .toLowerCase()}, qual horário (HH:mm)?`,
-            {
-              reply_to_message_id: answerRoleDate.message_id,
-              reply_markup: {
-                remove_keyboard: true,
-                selective: true
-              }
-            }
-          )
-        } else {
-          bot.sendMessage(
-            msg.chat.id,
-            `Use os botões, energúmeno ${
-              emoji.find('face_with_rolling_eyes').emoji
-            }`,
-            {
-              reply_to_message_id: answerRoleDate.message_id,
-              reply_markup: {
-                remove_keyboard: true,
-                selective: true
-              }
-            }
-          )
-        }
-      }
-    })
+    }
+  })
 })
 
 bot.onText(/^\/niver\b/i, msg => {
   if (tararaus.filter(tararau => tararau.userId === msg.from.id).length !== 0) {
-    bot.sendMessage(msg.chat.id, 'Você já inseriu sua data de nascimento', {
-      reply_to_message_id: msg.message_id,
-      reply_markup: {
-        remove_keyboard: true,
-        selective: true
-      }
-    })
+    bot.sendMessage(msg.chat.id, 'Você já inseriu sua data de nascimento', defaultKb(msg.message_id))
   } else {
     bot
       .sendMessage(
@@ -183,27 +130,16 @@ bot.onText(/^\/niver\b/i, msg => {
         `Por gentileza, insira a data (DD/MM/AAAA) em que sua mãe te pariu ${
           emoji.find('slightly_smiling_face').emoji
         }`,
-        {
-          reply_to_message_id: msg.message_id,
-          reply_markup: {
-            force_reply: true,
-            remove_keyboard: true,
-            selective: true
-          }
-        }
+        defaultKb(msg.message_id, true)
       )
       .then(() => {
         answerCallbacks[`${msg.chat.id}:${msg.from.id}`] = answerBirthdate => {
-          if (
-            moment(answerBirthdate.text, 'D/M/YYYY', 'pt-br', true).isValid()
-          ) {
+          if (moment(answerBirthdate.text, 'D/M/YYYY', 'pt-br', true).isValid()) {
             const date = moment(answerBirthdate.text, 'D/M/YYYY')
             bot
               .sendMessage(
-                msg.chat.id,
-                `Você nasceu dia ${date
-                  .format('D [de] MMMM [de] YYYY [(]dddd[)]')
-                  .toLowerCase()}?`,
+                chatId,
+                `Você nasceu dia ${date.format('D [de] MMMM [de] YYYY [(]dddd[)]').toLowerCase()}?`,
                 {
                   reply_to_message_id: answerBirthdate.message_id,
                   reply_markup: {
@@ -216,13 +152,9 @@ bot.onText(/^\/niver\b/i, msg => {
                 }
               )
               .then(() => {
-                answerCallbacks[
-                  `${msg.chat.id}:${msg.from.id}`
-                ] = answerConfirmation => {
+                answerCallbacks[`${msg.chat.id}:${msg.from.id}`] = answerConfirmation => {
                   if (answerConfirmation.text === 'Certamente') {
-                    const sign = getSign(date).filter(signEl =>
-                      date.within(signEl.range)
-                    )[0]
+                    const sign = getSign(date).filter(signEl => date.within(signEl.range))[0]
                     tararaus.push({
                       userId: msg.from.id,
                       userName: msg.from.first_name,
@@ -232,9 +164,7 @@ bot.onText(/^\/niver\b/i, msg => {
                     })
                     bot.sendMessage(
                       msg.chat.id,
-                      `Data registrada com sucesso... não sabia que seu signo era ${
-                        sign.name
-                      } ${sign.symbol}`,
+                      `Data registrada com sucesso... não sabia que seu signo era ${sign.name} ${sign.symbol}`,
                       {
                         reply_to_message_id: answerConfirmation.message_id,
                         reply_markup: {
@@ -244,32 +174,24 @@ bot.onText(/^\/niver\b/i, msg => {
                       }
                     )
                   } else {
-                    bot.sendMessage(
-                      msg.chat.id,
-                      'Repita o processo e vê se não erra dessa vez',
-                      {
-                        reply_to_message_id: answerConfirmation.message_id,
-                        reply_markup: {
-                          remove_keyboard: true,
-                          selective: true
-                        }
+                    bot.sendMessage(msg.chat.id, 'Repita o processo e vê se não erra dessa vez', {
+                      reply_to_message_id: answerConfirmation.message_id,
+                      reply_markup: {
+                        remove_keyboard: true,
+                        selective: true
                       }
-                    )
+                    })
                   }
                 }
               })
           } else {
-            bot.sendMessage(
-              msg.chat.id,
-              'Data inválida, preste atenção no formato',
-              {
-                reply_to_message_id: answerBirthdate.message_id,
-                reply_markup: {
-                  remove_keyboard: true,
-                  selective: true
-                }
+            bot.sendMessage(msg.chat.id, 'Data inválida, preste atenção no formato', {
+              reply_to_message_id: answerBirthdate.message_id,
+              reply_markup: {
+                remove_keyboard: true,
+                selective: true
               }
-            )
+            })
           }
         }
       })
