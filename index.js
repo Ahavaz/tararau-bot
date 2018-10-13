4e6e44c52cbdbb37d14009398ba726dc620d360e
Ahavaz <lucas.cesar.8@hotmail.com>
2018-10-13 01:13:32 -0300

    Fixed niver command bug

--- index.js
+++ index.js
@@ -115,7 +115,7 @@ bot.onText(/^\/niver\b/i, msg => {
   const userFullName = `${msg.from.first_name} ${msg.from.last_name || ''}`.trim()
   const userName = `[${userFullName}](tg://user?id=${userId})`
 
-  if (!tararaus.filter(tararau => tararau.chatId === chatId && tararau.userId === userId).length) {
+  if (tararaus.filter(tararau => tararau.chatId === chatId && tararau.userId === userId).length) {
     bot.sendMessage(chatId, 'Você já registrou sua data de nascimento', defaultKb(msgId))
   } else {
     bot
