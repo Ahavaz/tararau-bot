e407c695f75f89a40dc8c47ca4710c44867759b3
Ahavaz <lucas.cesar.8@hotmail.com>
2018-10-18 18:02:52 -0300

    Add mongodb config file

---
+++ config/mongodb.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const mongoUri = process.env.MONGODB_URI
+
+mongoose
+  .connect(
+    mongoUri,
+    { useNewUrlParser: true }
+  )
+  .catch(() => {
+    const msg = `ERROR: Não foi possível se conectar com o MongoDB!`
+    console.log(`\x1b[41m%s\x1b[37m`, msg, `\x1b[0m`)
+  })
