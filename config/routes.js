fbae62e2676e7d04c3e9f57d8f4779739de0e53c
Ahavaz <lucas.cesar.8@hotmail.com>
2018-10-20 09:47:55 -0300

    Add chat parameter in tararaus route

--- config/routes.js
+++ config/routes.js
@@ -1,11 +1,13 @@
 module.exports = app => {
-  app
-    .route('/roles')
-    .post(app.api.role.save)
-    .get(app.api.role.get)
+  // app.route('/').get((req, res) => res.send(`It's working!`))
+
+  // app
+  //   .route('/roles')
+  //   .post(app.api.role.save)
+  //   .get(app.api.role.get)
 
   app
-    .route('/tararaus')
+    .route('/tararaus/:chat')
     .post(app.api.tararau.save)
     .get(app.api.tararau.get)
 }
