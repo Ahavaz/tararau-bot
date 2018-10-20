12602a951088a92194a95c07389782dabe2820a4
Ahavaz <lucas.cesar.8@hotmail.com>
2018-10-20 10:00:22 -0300

    Add save and get methods in tararaus route

--- api/tararau.js
+++ api/tararau.js
@@ -1,21 +1,35 @@
 module.exports = app => {
   const Tararau = app.mongoose.model('Tararau', {
-    chatId: Number,
-    userId: Number,
-    userName: String,
-    userFullName: String,
-    signName: String,
-    signSymbol: String,
-    birthdate: Date
+    chatId: { type: Number, required: true },
+    userId: { type: Number, required: true },
+    userName: { type: String, required: true },
+    userFullName: { type: String, required: true },
+    signName: { type: String, required: true },
+    signSymbol: { type: String, required: true },
+    birthdate: { type: Date, required: true }
   })
 
-  const post = (req, res) => {
-    Tararau.save().then(tararau => res.json(tararau))
+  const save = (req, res) => {
+    const tararau = new Tararau({ ...req.body })
+
+    console.log('POST request', tararau)
+
+    tararau
+      .save()
+      .then(() => res.status(204).send())
+      .catch(err => res.status(500).send(err))
   }
 
   const get = (req, res) => {
-    Tararau.findOne().then(tararau => res.json(tararau))
+    const params = { ...req.params }
+
+    console.log('GET request', params)
+
+    Tararau.find({ chatId: params.chat })
+      .lean()
+      .then(tararau => res.json(tararau))
+      .catch(err => res.status(500).send(err))
   }
 
-  return { Tararau, post, get }
+  return { save, get }
 }
