4232f7e4c000ef2f9e83e916cd49277fcd58c2bf
Ahavaz <lucas.cesar.8@hotmail.com>
2018-10-21 06:06:51 -0300

    Add cronJob

---
+++ cronJob.js
@@ -0,0 +1,8 @@
+const schedule = require('node-schedule')
+const axios = require('axios')
+const { baseApiUrl } = require('./global')
+
+schedule.scheduleJob('*/10 6-0 * * *', async () => {
+  await axios.get(`${baseApiUrl}`)
+})
+schedule.scheduledJobs()
