d9a913cc0420dd2ab570a7983ca4e5f663ddc7de
Ahavaz <lucas.cesar.8@hotmail.com>
2018-10-21 01:48:44 -0300

    Implement roles api

--- api/role.js
+++ api/role.js
@@ -1,44 +1,35 @@
 module.exports = app => {
-  const Role = app.mongoose.model('Niver', {
-    chatId: Number,
-    userId: Number,
-    userName: String,
-    userFullName: String,
-    signName: String,
-    signSymbol: String,
-    birthdate: Date
+  const Role = app.mongoose.model('Role', {
+    chatId: { type: Number, required: true },
+    title: { type: String, required: true },
+    date: { type: Date, required: true },
+    location: { type: String, required: true }
   })
-  const save = async (req, res) => {
-    const role = { ...req.body }
-    if (req.params.id) role.id = req.params.id
 
-    try {
-      const roleFromDB = await app
-        .db('roles')
-        .where({ date: role.date })
-        .first()
-      if (!role.date) console.log(roleFromDB, 'Role já marcado')
-    } catch (msg) {
-      return res.status(400).send(msg)
-    }
+  const save = (req, res) => {
+    const role = new Role({ ...req.body })
 
-    if (role.id) {
-      app
-        .db('role')
-        .update(role)
-        .where({ id: role.id })
-        .then(() => res.status(204).send())
-        .catch(err => res.status(500).send(err))
-    } else {
-      app
-        .db('role')
-        .insert(role)
-        .then(() => res.status(204).send())
-        .catch(err => res.status(500).send(err))
-    }
-
-    return true
+    console.log('POST request', role)
+    return role.chatId && role.title
+      ? role
+          .save()
+          .then(() => res.status(204).send())
+          .catch(err => res.status(500).send(err))
+      : Role.deleteOne({ date: role.date, location: role.location })
+          .then(() => res.status(204).send())
+          .catch(err => res.status(500).send(err))
   }
 
-  return { Role, save }
+  const get = (req, res) => {
+    const params = { ...req.params }
+
+    console.log('GET request', params)
+
+    Role.find({ chatId: params.chat })
+      .lean()
+      .then(role => res.json(role))
+      .catch(err => res.status(500).send(err))
+  }
+
+  return { save, get }
 }
