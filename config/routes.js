5ab1b960de091060c010fc069dd2f63641b702d5
Ahavaz <lucas.cesar.8@hotmail.com>
2018-10-21 01:47:51 -0300

    Add roles route

--- config/routes.js
+++ config/routes.js
@@ -1,10 +1,8 @@
 module.exports = app => {
-  // app.route('/').get((req, res) => res.send(`It's working!`))
-
-  // app
-  //   .route('/roles')
-  //   .post(app.api.role.save)
-  //   .get(app.api.role.get)
+  app
+    .route('/roles/:chat')
+    .post(app.api.role.save)
+    .get(app.api.role.get)
 
   app
     .route('/tararaus/:chat')
