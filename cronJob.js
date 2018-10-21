43ae5393232c20b5bf14568e7311faed62386d0b
Ahavaz <lucas.cesar.8@hotmail.com>
2018-10-21 06:38:00 -0300

    Update cronJob

--- cronJob.js
+++ cronJob.js
@@ -2,6 +2,6 @@ const schedule = require('node-schedule')
 const axios = require('axios')
 const { baseApiUrl } = require('./global')
 
-schedule.scheduleJob('*/10 * * * * *', async () => {
+schedule.scheduleJob('*/10 * 6 * * *', async () => {
   await axios.get(`${baseApiUrl}`).then(() => console.log(`Request sent`))
 })
