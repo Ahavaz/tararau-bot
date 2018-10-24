c479b3b894934b443fb4c3f470a4f47309eb1bda
Ahavaz <lucas.cesar.8@hotmail.com>
2018-10-24 07:57:03 -0300

    Add congratulate message

--- cronJob.js
+++ cronJob.js
@@ -1,6 +1,7 @@
 const schedule = require('node-schedule')
 const axios = require('axios')
 const { baseApiUrl } = require('./global')
+const { congratulate } = require('./utils')
 
 // Call base url every 10min from 6am to 11:50pm to prevent app from sleeping in heroku
 schedule.scheduleJob('*/10 6-23 * * *', async () => {
@@ -9,10 +10,16 @@ schedule.scheduleJob('*/10 6-23 * * *', async () => {
 
 // Check for bdays every day at 8am to notify users and groups
 schedule.scheduleJob('*/10 * * * * *', async () => {
-  await axios.get(`${baseApiUrl}`).then(() => console.log(`Get all tararaus`))
-
-  // const { data } = await axios.get(`${baseApiUrl}/tararaus/${chatId}`)
   const { data } = await axios.get(`${baseApiUrl}/tararaus`)
   const tararaus = data
-  console.log(tararaus)
+  const chats = tararaus.reduce((array, tararau) => {
+    if (!array.includes(tararau.chatId)) array.push(tararau.chatId)
+    return array
+  }, [])
+
+  chats.forEach(chatId => {
+    tararaus.filter(tararau => tararau.chatId === chatId).forEach(tararau => {
+      congratulate(tararau)
+    })
+  })
 })
