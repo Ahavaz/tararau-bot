4f53b0cff4a999cf5f3efc066582583cf54c4a81
Ahavaz <lucas.cesar.8@hotmail.com>
2020-10-31 04:43:46 -0300

    Style(cronjob)

--- src/cronJob.js
+++ src/cronJob.js
@@ -4,12 +4,12 @@ const { congratulate } = require('./utils')
 
 // Call base url every 10min from 6am to 11:50pm to prevent app from sleeping in heroku
 schedule.scheduleJob('*/10 * * * *', async () => {
-  await axios.get().then(() => console.log(`Call base url`))
+  await axios.get().then(() => console.log('Call base url'))
 })
 
 // Check for bdays every day at 8am to notify users and groups
 schedule.scheduleJob('0 8 * * *', async () => {
-  const { data } = await axios.get(`/tararaus`)
+  const { data } = await axios.get('/tararaus')
   const tararaus = data
   const chats = tararaus.reduce((array, tararau) => {
     if (!array.includes(tararau.chatId)) array.push(tararau.chatId)
