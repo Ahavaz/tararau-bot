059a0b892304ff933b3cf7833c034cd96248c8be
Lucas Machado <lucas.cesar.8@hotmail.com>
2022-11-01 00:18:47 +0100

    refactor(server): Removes unnecessary middlewares (#86)

--- src/config/middlewares.ts
+++
@@ -1,8 +0,0 @@
-import cors from 'cors';
-import express, { Express } from 'express';
-
-module.exports = (app: Express): void => {
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  app.use(cors());
-};
